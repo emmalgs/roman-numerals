b2d27224e70e4a97dff2c5529cc7c8b3b4f1bbbf
Emma Gerigscott <elgerig@gmail.com>
2023-04-01 17:08:01 -0700

    test 3 passes, returns correct corresponding value to symbol

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Expected Output: 1;
 Test: "It should return 5 when V is entered"
 Code: romanizer("V");
 Expected Output: 5;
+
+Test: "It should return the corresponding value to the symbol"
+Code: romanizer("X")
+Expected Output: 10;
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 function romanizer(numeral) {
   if (numeral === "I") {
-    return 1
+    return 1;
   } else if (numeral === "V") {
-    return 5
+    return 5;
+  } else if (numeral === "X") {
+    return 10;
+  } else if (numeral === "L") {
+    return 50;
+  } else if (numeral === "C") {
+    return 100;
+  } else if (numeral === "D") {
+    return 500;
+  } else if (numeral === "M") {
+    return 1000;
   }
 }
