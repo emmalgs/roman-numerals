0473f5942a8df8cc52d8f5055dd20d8332dc46e1
Emma Gerigscott <elgerig@gmail.com>
2023-04-02 14:30:51 -0700

    UI logic to display romanizer output

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,58 @@
-const valuesArray = [
-  {
-    arabic: 1000,
-    roman: "M"
-  },
-  {
-    arabic: 900,
-    roman: "CM"
-  },
-  {
-    arabic: 500,
-    roman: "D"
-  },
-  {
-    arabic: 400,
-    roman: "CD"
-  },
-  {
-    arabic: 100,
-    roman: "C"
-  },
-  {
-    arabic: 90,
-    roman: "XC"
-  },
-  {
-    arabic: 50,
-    roman: "L"
-  },
-  {
-    arabic: 40,
-    roman: "XL"
-  },
-  {
-    arabic: 10,
-    roman: "X"
-  },
-  {
-    arabic: 9,
-    roman: "IX"
-  },
-  {
-    arabic: 5,
-    roman: "V"
-  },
-  {
-    arabic: 4,
-    roman: "IV"
-  },
-  {
-    arabic: 1,
-    roman: "I"
-  }
-]
-
 function romanizer(num) {
+  const valuesArray = [
+    {
+      arabic: 1000,
+      roman: "M"
+    },
+    {
+      arabic: 900,
+      roman: "CM"
+    },
+    {
+      arabic: 500,
+      roman: "D"
+    },
+    {
+      arabic: 400,
+      roman: "CD"
+    },
+    {
+      arabic: 100,
+      roman: "C"
+    },
+    {
+      arabic: 90,
+      roman: "XC"
+    },
+    {
+      arabic: 50,
+      roman: "L"
+    },
+    {
+      arabic: 40,
+      roman: "XL"
+    },
+    {
+      arabic: 10,
+      roman: "X"
+    },
+    {
+      arabic: 9,
+      roman: "IX"
+    },
+    {
+      arabic: 5,
+      roman: "V"
+    },
+    {
+      arabic: 4,
+      roman: "IV"
+    },
+    {
+      arabic: 1,
+      roman: "I"
+    }
+  ]
   if (num > 4000) {
     return false
   }
@@ -67,3 +66,22 @@ function romanizer(num) {
   });
   return romanNumeral;
 }
+
+function userInput(e) {
+  e.preventDefault();
+  resetDisplay();
+  const input = document.getElementById("input").value;
+  const output = romanizer(input);
+  
+  const display = document.getElementById("output");
+  display.innerText = output;
+}
+
+function resetDisplay() {
+  const output = document.getElementById("output");
+  output.innerText = '';
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", userInput)
+})
